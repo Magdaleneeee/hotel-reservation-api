7a4f076236d5ced93b916e2822a18873648c6d31
Magdalene Matey <magdalenematey8@gmail.com>
2026-01-03 08:35:15 -0800

    feat: add rooms list API

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
 
-# Create your views here.
+class RoomListView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
