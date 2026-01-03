ac177b53a2bb8bd026824bbc3a1974e3bd3db98a
Magdalene Matey <magdalenematey8@gmail.com>
2026-01-03 12:04:09 -0800

    feat: filter rooms by capacity

--- rooms/views.py
+++ rooms/views.py
@@ -3,5 +3,11 @@ from .models import Room
 from .serializers import RoomSerializer
 
 class RoomListCreateView(generics.ListCreateAPIView):
-    queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+    def get_queryset(self):
+        qs = Room.objects.all()
+        capacity = self.request.query_params.get('capacity')
+        if capacity:
+            qs = qs.filter(capacity__gte=capacity)
+        return qs
